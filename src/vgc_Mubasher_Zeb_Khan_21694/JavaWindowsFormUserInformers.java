ad39a299fc4afc5486f07bb7072c9f2e79cb9796
Mubasher <21694@student.dorset-college.ie>
2021-04-29 15:29:29 +0100

    Users and login form

---
+++ src/vgc_Mubasher_Zeb_Khan_21694/JavaWindowsFormUserInformers.java
@@ -0,0 +1,11 @@
+package vgc_Mubasher_Zeb_Khan_21694;
+
+import javax.swing.JOptionPane;
+
+public class JavaWindowsFormUserInformers {
+
+	public static void showMsgWithJPane(String msg)
+	{
+		JOptionPane.showMessageDialog(null, msg);
+	}
+}
